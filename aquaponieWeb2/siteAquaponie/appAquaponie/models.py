0e069a97993153d6877ba5b58324156cc8d78a31
JimmyJunor <73443903+JimmyJunor@users.noreply.github.com>
2024-03-20 12:10:48 -0400

    Add files via upload

--- aquaponieWeb2/siteAquaponie/appAquaponie/models.py
+++ aquaponieWeb2/siteAquaponie/appAquaponie/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class mesure(models.Model):
+    humidite = models.DecimalField(max_digits=5, decimal_places=2)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    ph = models.DecimalField(max_digits=5, decimal_places=2)
+    niveauDeau = models.DecimalField(max_digits=5, decimal_places=2)
+    lumiere = models.IntegerField()
+
+    def __str__(self):
+        return {self.pk}
--- aquaponieWeb2/siteAquaponie/appAquaponie/views.py
+++ aquaponieWeb2/siteAquaponie/appAquaponie/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+#from models import mesure
 
 
 def index(request):
+    #dataTemperature = mesure.temperature
+    #dataHumidite = mesure.humidite
+    #dataPh = mesure.ph
+    #dataLumiere = mesure.lumiere
+    #dataNiveauDeau = mesure.niveauDeau
     return render(request, 'appAquaponie/index.html', {})

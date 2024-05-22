39f8c481674e4d313be20c5a855ae1ec25c1cad9
WishPib <150097552+WishPib@users.noreply.github.com>
2024-05-21 22:13:57 -0400

    Update Main.py commentaire

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import CapteurNiveauEau
 import CapteurMOIST
 import BaseDonne
 import Pompe
-# Utiliser "python3 manage.py runserver 7000" dans le terminal en étant dans le bon directory pour lancer le serveur local
+# Utiliser "python3 manage.py runserver 7000" dans le terminal en étant dans le bon directory (aquaponieWeb2/siteAquaponie) pour lancer le serveur local
 # et ouvrire l'adresse donnée
 time.sleep(1)
 valeurNiveauEau = False
